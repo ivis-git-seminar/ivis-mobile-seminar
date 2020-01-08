082ced3e7a797094a40496f8c09b297ef0b11a00
JHLEE-ICT <jhlee.ict@gmail.com>
2020-01-08 20:40:04 +0900

    feat: add part2-of-main

--- tetris/main.js
+++ tetris/main.js
@@ -45,3 +45,76 @@ function initNext() {
     ctxNext.canvas.height = 4 * BLOCK_SIZE;
     ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE);
 }
+
+function initNext() {
+    // Calculate size of canvas from constants.
+    ctxNext.canvas.width = 4 * BLOCK_SIZE;
+    ctxNext.canvas.height = 4 * BLOCK_SIZE;
+    ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE);
+}
+
+function addEventListener() {
+    document.addEventListener('keydown', event => {
+        if (event.keyCode === KEY.P) {
+        pause();
+    }
+    if (event.keyCode === KEY.ESC) {
+        gameOver();
+    } else if (moves[event.keyCode]) {
+        event.preventDefault();
+        // Get new state
+        let p = moves[event.keyCode](board.piece);
+        if (event.keyCode === KEY.SPACE) {
+            // Hard drop
+            while (board.valid(p)) {
+                account.score += POINTS.HARD_DROP;
+                board.piece.move(p);
+                p = moves[KEY.DOWN](board.piece);
+            }
+            board.piece.hardDrop();
+        } else if (board.valid(p)) {
+            board.piece.move(p);
+            if (event.keyCode === KEY.DOWN) {
+                account.score += POINTS.SOFT_DROP;
+            }
+        }
+    }
+});
+}
+
+function resetGame() {
+    account.score = 0;
+    account.lines = 0;
+    account.level = 0;
+    board.reset();
+    time = { start: 0, elapsed: 0, level: LEVEL[account.level] };
+}
+
+function play() {
+    addEventListener();
+    resetGame();
+    time.start = performance.now();
+    // If we have an old game running a game then cancel the old
+    if (requestId) {
+        cancelAnimationFrame(requestId);
+    }
+
+    animate();
+}
+
+function animate(now = 0) {
+    time.elapsed = now - time.start;
+    if (time.elapsed > time.level) {
+        time.start = now;
+        if (!board.drop()) {
+            gameOver();
+            return;
+        }
+    }
+
+    // Clear board before drawing new state.
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    board.draw();
+    requestId = requestAnimationFrame(animate);
+}
