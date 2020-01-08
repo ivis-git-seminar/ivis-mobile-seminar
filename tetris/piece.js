ecde9d00dadd150e21f47d84600aac1cbf5b7d7a
kim-donguk <dkfnfn9871@gmail.com>
2020-01-08 20:13:50 +0900

    feat: add piece.js

---
+++ tetris/piece.js
@@ -0,0 +1,33 @@
+class Piece {
+  x;
+  y;
+  color;
+  shape;
+  ctx;
+  typeId;
+  hardDropped;
+
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.spawn();
+  }
+
+  spawn() {
+    this.typeId = this.randomizeTetrominoType(COLORS.length - 1);
+    this.shape = SHAPES[this.typeId];
+    this.color = COLORS[this.typeId];
+    this.x = 0;
+    this.y = 0;
+    this.hardDropped = false;
+  }
+
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
+    });
+  }
